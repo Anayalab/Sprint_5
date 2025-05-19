987fe8cce9de637d18589b566ee71a29eb4377ba
Andrey Labazin <alabazin@astralinux.ru>
2025-05-19 20:12:29 +0300

    Убраны неиспользуемые импорты

--- src/test/java/com/example/AlexLionTest.java
+++ src/test/java/com/example/AlexLionTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AlexLionTest {
